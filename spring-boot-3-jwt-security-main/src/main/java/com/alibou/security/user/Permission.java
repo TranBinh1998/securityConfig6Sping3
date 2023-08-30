bb520ed6df95c69726c87c5cb4b04ea4db7acd98
Tran Binh fpt <tranbinhit19998@gmail.com>
2023-08-30 21:09:25 +0700

    update30/08

---
+++ spring-boot-3-jwt-security-main/src/main/java/com/alibou/security/user/Permission.java
@@ -0,0 +1,23 @@
+package com.alibou.security.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum Permission {
+
+//    USER_RECEIVE_COUPON("user:"),
+    ADMIN_READ("admin:read"),
+    ADMIN_UPDATE("admin:update"),
+    ADMIN_CREATE("admin:create"),
+    ADMIN_DELETE("admin:delete"),
+    MANAGER_READ("management:read"),
+    MANAGER_UPDATE("management:update"),
+    MANAGER_CREATE("management:create"),
+    MANAGER_DELETE("management:delete")
+
+    ;
+
+    @Getter
+    private final String permission;
+}
